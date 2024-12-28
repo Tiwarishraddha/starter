d61836a35cb26c8f3a589bf6f0d0d5585eec7141
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2024-12-28 18:11:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Testimonial from "./components/Testimonial"
+import reviews from "./data";
+const App = () => {
+  return (
+  <div>
+    <div><h1>Our Testimonials</h1>
+    <div></div>
+    <Testimonial reviews={reviews}/>
+    </div>
+  </div>
+  );
+};
+
+export default App;
